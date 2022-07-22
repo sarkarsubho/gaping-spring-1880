a12ef3e7309085f6fe3a0ddb14926175ef9dd93f
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-07-22 12:10:00 +0530

    update

---
+++ buffer-app/src/Components/PublishPageComponents/PublishInstaSection395.jsx
@@ -0,0 +1,30 @@
+import { Box, Container, Flex } from '@chakra-ui/react'
+import React from 'react'
+import instagram from "../data/publish-instragram-stories.jpg"
+const PublishInstaSection395 = () => {
+  return (
+    <Flex flexDirection={'column'}>
+        {/* Instagram */}
+        <Container>
+            <Flex>
+                <Box>
+                    HELLO
+
+                </Box>
+                <Box>
+                    <img src={instagram} alt="" />
+                </Box>
+            </Flex>
+        </Container>
+
+
+            {/* Icons */}
+        <Container>
+
+        </Container>
+
+    </Flex>
+  )
+}
+
+export default PublishInstaSection395
--- buffer-app/src/Pages/PublishPage.jsx
+++ buffer-app/src/Pages/PublishPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import styled from "styled-components"
+import PublishInstaSection395 from '../Components/PublishPageComponents/PublishInstaSection395'
+// import styled from "styled-components"
 import PublishIntro395 from '../Components/PublishPageComponents/PublishIntro395'
 import PublishPageClients395 from '../Components/PublishPageComponents/PublishPageClients395'
 
@@ -7,33 +8,16 @@ const PublishPage = () => {
   return (
     <div>
         {/* Intro*/}
-        {/* <section>
-          Intro
-          <p>Build your audience and grow your brand on social media<br/>
-            Plan and publish your content for Instagram, TikTok, Facebook, Twitter, Pinterest, and LinkedIn, all from one simple dashboard.</p>
-        <button>getStarted</button>
-        <video autoPlay loop muted>
-            <source />
-        </video>
-        </section> */}
+        
         <PublishIntro395/>
-        {/* Client5 */}
-        {/* <section>
-            Client
-            <h2>imags of logo for display</h2>
-            <h3>Craft the perfect post for each social network, all in one place <br/>
-           Plan and publish your content for Instagram, Facebook, Twitter, Pinterest, and LinkedIn, all     one simple dashboard.</h3>
-<img alt="calendar-image"></img>
-        </section> */}
-        <PublishPageClients395/>
         {/* Plan and Dashboard */}
-        <section>
-          <h3>Plan and Dashboard</h3>
-        </section>
+        {/* Client */}
+        
+        <PublishPageClients395/>
+        
         {/* {Instagram Section} */}
-        <section>
-          <h3>Instagram Section</h3>
-        </section>
+        
+        <PublishInstaSection395/>
         {/* Colab section */}
         <section>
           <h3>Colab Section</h3>

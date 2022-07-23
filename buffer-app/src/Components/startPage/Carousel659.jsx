576001b758cd6cb30463ac9bebed4ac727f2d4ff
Deepak yadav <deepaky541.dy@gmail.com>
2022-07-23 15:18:09 +0530

    update

---
+++ buffer-app/src/Components/startPage/Carousel659.jsx
@@ -0,0 +1,332 @@
+import React from 'react'
+import {
+  Heading,
+  Avatar,
+  Box,
+  Flex,
+  Text,
+  Stack,
+  useColorModeValue,
+  Container,
+  Image
+} from "@chakra-ui/react";
+import styles from "./Carousel.module.css";
+
+export const Carousel659 = () => {
+  return (
+    <Container
+      maxWidth={["500px", "800px", "1000px", "1200px", "1500px", "1500px"]}
+      mt="60px"
+      padding="0"
+    >
+      <div className={styles.slider}>
+        <div className={styles.slidertrack}>
+          {/* box 1 */}
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-5.webp"></Image>
+          </Box>
+
+          {/* box 2 */}
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-6.webp"></Image>
+          </Box>
+
+          {/* box 3 */}
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-7.webp"></Image>
+          </Box>
+
+          {/* box 4 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-8.webp"></Image>
+          </Box>
+
+          {/* box 5 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-9.webp"></Image>
+          </Box>
+          {/* box 6 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-11.webp"></Image>
+          </Box>
+          {/* box 7 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-12.webp"></Image>
+          </Box>
+          {/* box 8 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-13.webp"></Image>
+          </Box>
+          {/* box 9 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-1.webp"></Image>
+          </Box>
+          {/* box 10 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-2.webp"></Image>
+          </Box>
+
+          {/* box 1 */}
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-5.webp"></Image>
+          </Box>
+
+          {/* box 2 */}
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-6.webp"></Image>
+          </Box>
+
+          {/* box 3 */}
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-7.webp"></Image>
+          </Box>
+
+          {/* box 4 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-8.webp"></Image>
+          </Box>
+
+          {/* box 5 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-9.webp"></Image>
+          </Box>
+          {/* box 6 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-11.webp"></Image>
+          </Box>
+          {/* box 7 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-12.webp"></Image>
+          </Box>
+          {/* box 8 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-13.webp"></Image>
+          </Box>
+          {/* box 9 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-1.webp"></Image>
+          </Box>
+          {/* box 10 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-2.webp"></Image>
+          </Box>
+          {/* box 1 */}
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-5.webp"></Image>
+          </Box>
+
+          {/* box 2 */}
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-6.webp"></Image>
+          </Box>
+
+          {/* box 3 */}
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-7.webp"></Image>
+          </Box>
+
+          {/* box 4 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-8.webp"></Image>
+          </Box>
+
+          {/* box 5 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-9.webp"></Image>
+          </Box>
+          {/* box 6 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-11.webp"></Image>
+          </Box>
+          {/* box 7 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-12.webp"></Image>
+          </Box>
+          {/* box 8 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-13.webp"></Image>
+          </Box>
+          {/* box 9 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-1.webp"></Image>
+          </Box>
+          {/* box 10 */}
+
+          <Box
+            maxW={"390px"}
+            w={"full"}
+            className={`${styles.slide}`}
+            rounded={"md"}
+          >
+            <Image src="https://buffer.com/static/start-page/webp/start-page-2.webp"></Image>
+          </Box>
+        </div>
+      </div>
+    </Container>
+  );
+}
--- buffer-app/src/Components/startPage/StartPageIntro659.jsx
+++ buffer-app/src/Components/startPage/StartPageIntro659.jsx
@@ -1,19 +1,32 @@
-import { Box, Button, Flex, Heading,Text } from '@chakra-ui/react'
+import { Box, Button, Container, Flex, Heading,Text } from '@chakra-ui/react'
 import React from 'react'
 
 const StartPageIntro659 = () => {
   return (
-    <Flex width={["100%","100%","45%","45%"]} alignItems="center" gap="40px"  direction="column" >
-      <Heading size="xl" fontWeight="extrabold" letterSpacing="2px" >
-        The link in bio that drives more traffic to your content for free
-      </Heading>
-      <Text>
-        Start Page is a simple, beautiful, flexible link in bio page that brings
-        together all of your best content in one place. Share it in your bios on
-        Instagram, YouTube, TikTok, or any other social profiles.
-      </Text>
-      <Button bgColor="blue.400" padding="40px" width={["100%","100%","50%","50%"]} >Create your Start Page for free</Button>
-    </Flex>
+    
+      <Flex
+        width={["100%", "100%", "45%", "45%"]}
+        alignItems="center"
+        gap="40px"
+        direction="column"
+      >
+        <Heading size="xl" fontWeight="extrabold" letterSpacing="2px">
+          The link in bio that drives more traffic to your content for free
+        </Heading>
+        <Text>
+          Start Page is a simple, beautiful, flexible link in bio page that
+          brings together all of your best content in one place. Share it in
+          your bios on Instagram, YouTube, TikTok, or any other social profiles.
+        </Text>
+        <Button
+          bgColor="blue.400"
+          padding="40px"
+          width={["100%", "100%", "50%", "50%"]}
+        >
+          Create your Start Page for free
+        </Button>
+      </Flex>
+    
   );
 }
 

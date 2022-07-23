d6fce20c18ef0ffc206bfbf5c06a9b2f50a9c6aa
Deepak yadav <deepaky541.dy@gmail.com>
2022-07-24 00:23:05 +0530

    update

--- buffer-app/src/App.js
+++ buffer-app/src/App.js
@@ -14,11 +14,11 @@ function App() {
   return (
     <div className="App">
 
-      <Container maxW={["100%", "100%", "100%", "100%"]} >
+     
         <Navbar></Navbar>
         <MainRoutes></MainRoutes>
         <Footer></Footer>
-      </Container>
+     
 
 
     </div>
